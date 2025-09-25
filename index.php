329526c431029190ac59c6bb337a79649808749e
Kaua-Mariano-Santinho <kauams.senai@gmail.com>
2025-09-25 15:20:33 -0300

    commit inicial

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Juros e Conversor</title>
+</head>
+<body>
+
+    <form action="conversor.php" method="post">
+        <select name="" id="">
+            <option value="Conversor">Conversor</option>
+            <option value="Juros">Juros</option>
+        </select>
+        <input type="submit" value="Conversor">
+    </form>
+    
+    
+</body>
+</html>
